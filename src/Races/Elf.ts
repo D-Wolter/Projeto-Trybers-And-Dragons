3223cc6a02e32617ad92c672f11347012ad8cd06
D-Wolter <wolter.daniel@gmail.com>
2023-05-05 16:31:38 -0300

    feat: req 2 criado classes que herdam Race Dwarf,Elf,Halfling-Orc

---
+++ src/Races/Elf.ts
@@ -0,0 +1,26 @@
+import Race from './Race';
+
+class Elf extends Race {
+  private _maxLifePoints: number;
+  private static _instances: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 99;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    if (!this._instances) {
+      this._instances = 1;
+      return this._instances;
+    }
+    this._instances += 1;
+    return this._instances;
+  }  
+}
+
+export default Elf;
---
+++ src/Races/Orc.ts
@@ -0,0 +1,26 @@
+import Race from './Race';
+
+class Dwarf extends Race {
+  private _maxLifePoints: number;
+  private static _instances: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 74;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    if (!this._instances) {
+      this._instances = 1;
+      return this._instances;
+    }
+    this._instances += 1;
+    return this._instances;
+  }  
+}
+
+export default Dwarf;
